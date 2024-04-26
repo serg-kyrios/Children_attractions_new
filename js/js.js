78ac23115c612b85af660f32aba46493dc104be8
Sergey KYRYLIUK <121867142+serg-kyrios@users.noreply.github.com>
2024-04-26 14:44:51 +0300

    first
    modal window + error for mobile + time countdown

---
+++ js/js.js
@@ -0,0 +1,12 @@
+'use strict';
+//console.log
+//console.log
+//console.log
+//console.log(arr.map()); // Світ (метод працює)
+
+//debugger;
+function slow(x) {
+    // тут можуть бути важкі ресурсозатратні операції
+    alert(`Викликана з ${x}`);
+    return x;
+}
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+ let count = 0;
+    function counter() {
+        count++;
+    }
+
+    console.log(counter()); // 0
+    console.log(counter()); // 1 // ... your code ...
